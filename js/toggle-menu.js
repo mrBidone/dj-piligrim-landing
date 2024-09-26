e6a00ddad4d06b91407173703193ab2708585eba
mrBidone <ab.bz@ukr.net>
2024-09-26 23:21:52 +0200

    Remove modal-window and Add new sub-menu

--- js/toggle-menu.js
+++ js/toggle-menu.js
@@ -1,13 +1,25 @@
+// Обработчик для кнопки "toggle-listening-menu"
 document
-  .getElementById("toggle-menu")
+  .getElementById("toggle-listening-menu")
   .addEventListener("click", function (event) {
     event.preventDefault(); // Отключаем переход по ссылке
 
     const menu = document.getElementById("listening-menu");
-    const listeningList = document.getElementById("listening-links-list");
-    const thumbIcon = document.getElementById("activeIconThumb");
+    const thumbIcon = this.querySelector(".activeIconThumb"); // Поиск иконки внутри текущей кнопки
 
     menu.classList.toggle("open");
-    // listeningList.classList.toggle("open");
+    thumbIcon.classList.toggle("isActive");
+  });
+
+// Обработчик для кнопки "toggle-booking-menu"
+document
+  .getElementById("toggle-booking-menu")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); // Отключаем переход по ссылке
+
+    const menu = document.getElementById("booking-menu");
+    const thumbIcon = this.querySelector(".activeIconThumb"); // Поиск иконки внутри текущей кнопки
+
+    menu.classList.toggle("open");
     thumbIcon.classList.toggle("isActive");
   });
