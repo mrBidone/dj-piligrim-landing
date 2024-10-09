5088954cfba1b455ac533a58772af96983d51d33
mrBidone <ab.bz@ukr.net>
2024-10-09 21:40:52 +0200

    Deleted all scrolling animations

--- js/scroll.js
+++ js/scroll.js
@@ -1,25 +1,14 @@
-window.addEventListener("scroll", function () {
-  const pageHeader = document.querySelector(".page-header");
-  const swiperSection = document.querySelector(".swiper-section");
-  const offersSection = document.querySelector(".offers-section");
-  const bgGradient = document.querySelector(".track-swiper-bg-graident");
-  const headerRect = pageHeader.getBoundingClientRect();
-  const offersRect = offersSection.getBoundingClientRect();
-  if (headerRect.bottom <= 0) {
-    swiperSection.classList.add("fixed");
-    offersSection.classList.add("js-offers-section");
-  } else {
-    swiperSection.classList.remove("fixed");
-    offersSection.classList.remove("js-offers-section");
-  }
+// window.addEventListener("scroll", function () {
+//   const pageHeader = document.querySelector(".page-header");
+//   const swiperSection = document.querySelector(".swiper-section");
+//   const offersSection = document.querySelector(".offers-section");
+//   const bgGradient = document.querySelector(".track-swiper-bg-graident");
+//   const headerRect = pageHeader.getBoundingClientRect();
+//   const offersRect = offersSection.getBoundingClientRect();
 
-  const pseudoElementHeight = 357;
-
-  if (offersRect.top <= pseudoElementHeight) {
-    bgGradient.classList.add("jsGradient");
-    offersSection.classList.add("fixed-before");
-  } else {
-    offersSection.classList.remove("fixed-before");
-    bgGradient.classList.remove("jsGradient");
-  }
-});
+//   if (headerRect.bottom <= 0) {
+//     swiperSection.classList.add("fixed");
+//   } else {
+//     swiperSection.classList.remove("fixed");
+//   }
+// });
